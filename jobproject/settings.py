2789e4652405e69a6a5d0ee7787ca5f948b59ed7
FloKots <florencekotohoyoh@gmail.com>
2022-06-21 13:11:00 +0300

    add images to profile pics

--- jobproject/settings.py
+++ jobproject/settings.py
@@ -33,12 +33,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'kazimtaani.apps.KazimtaaniConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
