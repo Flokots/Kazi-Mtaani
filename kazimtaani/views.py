e6dc03951a6b450149643e20c85a4aaf95b980bc
FloKots <florencekotohoyoh@gmail.com>
2022-06-22 11:08:38 +0300

    Create index url path

--- kazimtaani/views.py
+++ kazimtaani/views.py
@@ -1,12 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from kazimtaani.models import NewsLetterRecipients
 from .forms import NewsLetterForm
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
 
 def subscribe(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+
+            recipient=NewsLetterRecipients(name=name, email=email)
+            recipient.save()
+            HttpResponseRedirect('index')
             print('valid')
         else:
             form = NewsLetterForm()
