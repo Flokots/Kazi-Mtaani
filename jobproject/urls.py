5987cdaecb9c2bd67dda54727fb24865fdabdf4d
FloKots <florencekotohoyoh@gmail.com>
2022-06-22 10:56:16 +0300

    define subscribe path

--- jobproject/urls.py
+++ jobproject/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from users import views as user_views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -28,6 +29,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('api/profile/', user_views.ProfileList.as_view(), name='profile-list'),
+    path('', include('kazimtaani.urls')),
 ]
 
 if settings.DEBUG:
--- kazimtaani/views.py
+++ kazimtaani/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import NewsLetterForm
 
 # Create your views here.
+
+def subscribe(request):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+        else:
+            form = NewsLetterForm()
+        return render(request, 'subscribe.html', {'form': form})
